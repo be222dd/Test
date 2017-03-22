02a1da8efff28fa785e64ba922517516a388325d
be222dd <beysimeryilmaz@gmail.com>
2017-03-22 13:40:26 +0100

    Update HelloGoup.java

--- HelloGoup.java
+++ HelloGoup.java
@@ -4,7 +4,10 @@ public class HelloGoup
 {
 	public static void main(String[] args)
 	{
+		
 		System.out.println("Hello Group!");
 		System.out.println("Hello from mainAccount");
+		
+		System.out.println("Hello Group! I am beysim");
 	}
 }
